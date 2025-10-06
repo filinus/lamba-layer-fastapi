7810b62f1ebddefb4ec86e7550055b99ff2ecfed
Valeriy Filin <val@filin.us>
2025-10-05 19:41:22 -0700

    fastapi~=0.118.0

--- main.tf
+++ main.tf
@@ -22,11 +22,27 @@ provider "aws" {
 resource "aws_lambda_layer_version" "fastapi_layer" {
   filename          = "${path.module}/lambda-layer-fastapi.zip"  # путь к локальному архиву
   layer_name        = "fastapi-layer"
-  compatible_runtimes = ["python3.8", "python3.9", "python3.10", "python3.11", "python3.12", "python3.13"]
-  description       = "FastAPI and dependencies for AWS Lambda Python"
+  compatible_runtimes = ["python3.9", "python3.10", "python3.11", "python3.12", "python3.13"]
+  description       = "FastAPI 0.118.0 +Magnum+dependencies for AWS Lambda Python"
+  compatible_architectures = ["x86_64"]
 }
 
 output "lambda_layer_arn" {
   value = aws_lambda_layer_version.fastapi_layer.arn
 }
+
+data "aws_lambda_layer_version" "fastapi_layer" {
+  layer_name = "fastapi-layer"
+  compatible_architecture = "x86_64"
+}
+
+output "latest_fastapi_layer_generic_arn" {
+  value = data.aws_lambda_layer_version.fastapi_layer.layer_arn
+}
+
+output "latest_fastapi_layer_exact_arn" {
+  value = data.aws_lambda_layer_version.fastapi_layer.arn
+}
+
+
 
